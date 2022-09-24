9de58c432ee9ce94435e1b3b1a2d4e73868fd07a
okdol1 <okdol0611@gmail.com>
2022-09-24 22:43:33 +0900

    더미리스트 주석 제거 -> jsonplaceholder 주소

--- src/App.js
+++ src/App.js
@@ -4,36 +4,7 @@ import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList'
 import Lifecycle from './Lifecycle'
 
-// const dummyList = [
-//   {
-//     id: 1,
-//     author: "연은빈",
-//     content: "hi~~~!1",
-//     emotion: 5,
-//     created_date: new Date().getTime()
-//   },
-//   {
-//     id: 2,
-//     author: "연금빈",
-//     content: "hi~~~!22",
-//     emotion: 4,
-//     created_date: new Date().getTime()
-//   },
-//   {
-//     id: 3,
-//     author: "연동빈",
-//     content: "hi~~~!333",
-//     emotion: 3,
-//     created_date: new Date().getTime()
-//   },
-//   {
-//     id: 4,
-//     author: "김무성",
-//     content: "hi~~~!444",
-//     emotion: 2,
-//     created_date: new Date().getTime()
-//   },
-// ]
+// https://jsonplaceholder.typicode.com/comments
 
 function App() {
   const [data, setData] = useState([]);
